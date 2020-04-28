3de672bf0118b24b105811a9f94767f9016b99fc
sealanguage <elaine.froehlich@gmail.com>
2020-04-28 19:44:18 -0400

    fixing code practice from the weeks where I saved from jupyter notebook and got lots of crap code in the files

--- grades_calculator_v3.py
+++ grades_calculator_v3.py
@@ -1,87 +1,18 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 12,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " Enter your choice \n",
-      " Enter grades -> 1 \n",
-      " Exit program -> 6 \n",
-      " -> 5\n"
-     ]
-    },
-    {
-     "ename": "TypeError",
-     "evalue": "object of type 'bool' has no len()",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[0;31mTypeError\u001b[0m                                 Traceback (most recent call last)",
-      "\u001b[0;32m<ipython-input-12-df2dd311b919>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m\u001b[0m\n\u001b[1;32m     11\u001b[0m     \u001b[0mchoice\u001b[0m \u001b[0;34m=\u001b[0m \u001b[0minput\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0;34m\" -> \"\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     12\u001b[0m \u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m---> 13\u001b[0;31m     \u001b[0;32mif\u001b[0m \u001b[0mlen\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0mchoice\u001b[0m \u001b[0;34m==\u001b[0m \u001b[0;36m1\u001b[0m\u001b[0;34m)\u001b[0m \u001b[0;34m:\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0m\u001b[1;32m     14\u001b[0m         \u001b[0mprint\u001b[0m\u001b[0;34m(\u001b[0m\u001b[0;34m\"enter a valid choice number 1 - 6\"\u001b[0m\u001b[0;34m)\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m     15\u001b[0m         \u001b[0;32mcontinue\u001b[0m\u001b[0;34m\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n",
-      "\u001b[0;31mTypeError\u001b[0m: object of type 'bool' has no len()"
-     ]
-    }
-   ],
-   "source": [
-    "grades = []    #initialize an empty list\n",
-    "\n",
-    "while True :    #this program runs in an infinite loop\n",
-    "    print(\" Enter your choice \")\n",
-    "    print(\" Enter grades -> 1 \")\n",
-    "#     print(\" Delete grades -> 2 \")\n",
-    "#     print(\" \")\n",
-    "#     print(\" \")\n",
-    "    print(\" Exit program -> 6 \")\n",
-    "    \n",
-    "    choice = input(\" -> \")\n",
-    "    \n",
-    "    if len(choice == 1) :\n",
-    "        print(\"enter a valid choice number 1 - 6\")\n",
-    "        continue\n",
-    "    \n",
-    "    if int(choice) == 6 :\n",
-    "        break"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+grades = []    #initialize an empty list
+
+while True :    #this program runs in an infinite loop
+   print(" Enter your choice ")
+   print(" Enter grades -> 1 ")
+   print(" Delete grades -> 2 ")
+   print(" ")
+   print(" ")
+   print(" Exit program -> 6 ")
+
+   choice = input(" -> ")
+
+   if len(choice == 1) :
+      print("enter a valid choice number 1 - 6")
+      continue
+
+   if int(choice) == 6 :
+      break
--- grades_sum_averages.py
+++ grades_sum_averages.py
@@ -1,112 +1,20 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "SyntaxError",
-     "evalue": "unexpected EOF while parsing (<ipython-input-3-5d1e93a3e16f>, line 20)",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-3-5d1e93a3e16f>\"\u001b[0;36m, line \u001b[0;32m20\u001b[0m\n\u001b[0;31m    print(\"Average class grade is: \", average\u001b[0m\n\u001b[0m                                             ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m unexpected EOF while parsing\n"
-     ]
-    }
-   ],
-   "source": [
-    "#    print(\"Enter grades to calculate mean/average grades. tyep e to exit\")\n",
-    "\n",
-    "# create an empty list\n",
-    "grades = []\n",
-    "sum = 0\n",
-    "\n",
-    "while( True ) :\n",
-    "    grade = input(\" enter grades \")\n",
-    "    if grade == \"e\" :\n",
-    "        break\n",
-    "    else :\n",
-    "        grade = float(grade)\n",
-    "        grades.append(grade)\n",
-    "print(\"grades: \", grades)\n",
-    "        \n",
-    "\n",
-    "for grade in grades :\n",
-    "    sum = sum + grade\n",
-    "average = sum /len(grades)\n",
-    "print(\"Average class grade is: \", average"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 5,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      " enter grades 3.2\n",
-      " enter grades 2.1\n",
-      " enter grades 3.7\n",
-      " enter grades 3.3\n",
-      " enter grades 1.0\n",
-      " enter grades e\n",
-      "grades:  [3.2, 2.1, 3.7, 3.3, 1.0]\n",
-      "Average class grade is:  2.66\n"
-     ]
-    }
-   ],
-   "source": [
-    "#    print(\"Enter grades to calculate mean/average grades. tyep e to exit\")\n",
-    "\n",
-    "# create an empty list\n",
-    "grades = []\n",
-    "sum = 0\n",
-    "\n",
-    "while( True ) :\n",
-    "    grade = input(\" enter grades \")\n",
-    "    if grade == \"e\" :\n",
-    "        break\n",
-    "    else :\n",
-    "        grade = float(grade)\n",
-    "        grades.append(grade)\n",
-    "print(\"grades: \", grades)\n",
-    "        \n",
-    "\n",
-    "for grade in grades :\n",
-    "    sum = sum + grade\n",
-    "average = sum /len(grades)\n",
-    "print(\"Average class grade is: \", average)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.1"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+#  print("Enter grades to calculate mean/average grades. tyep e to exit")
+
+# create an empty list
+grades = []
+sum = 0
+
+while( True ) :
+   grade = input(" enter grades ")
+   if grade == "e" :
+      break
+   else :
+      grade = float(grade)
+      grades.append(grade)
+print("grades: ", grades)
+
+
+for grade in grades :
+   sum = sum + grade
+average = sum /len(grades)
+print("Average class grade is: ", average)
