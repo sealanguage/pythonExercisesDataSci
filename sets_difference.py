fbf7b5ff551ae0fc0cb71ba978a82f352aee7d7f
sealanguage <elaine.froehlich@gmail.com>
2020-06-30 22:48:31 -0400

    learning to use sets in python, learning to use tuples,  some algorithm solutions

---
+++ sets_difference.py
@@ -0,0 +1,6 @@
+odd_num1 = {1, 3, 5, 7, 9, 11}
+odd_num2 = {1, 3, 12}
+
+left = odd_num1.difference(odd_num2)
+
+left
---
+++ tuple_index_method.py
@@ -0,0 +1,4 @@
+ages =(4, 5, 7, 3, 4, 7, 8, 5, 8, 5)
+ages.index(7)
+
+# returns 2 for index of 7 in the tuple
