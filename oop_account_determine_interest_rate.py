f2a5fcd9b5c466f322aad1986e65aad7e8128e6e
sealanguage <elaine.froehlich@gmail.com>
2020-07-05 00:19:31 -0400

    learning oop principles in python using acc object and Account cass

---
+++ oop_account_determine_interest_rate.py
@@ -0,0 +1,19 @@
+class Account:
+    def __init__(self, number):
+        self.number = number
+    
+    def account_type(self):
+        #  get the account type
+        if str(self.number).startswith("1") :
+            self.type = "current"
+        elif str(self.number).startswith("2") :
+            self.type = "savings"
+    
+    def interest_rate(self):
+        
+        self.account_type()
+        if self.account_type == "current":
+            self.interest = 0
+        elif self.account_type == "savings":
+            self.interest = 5
+        return self.interest
