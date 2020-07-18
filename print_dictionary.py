7d9b3ac300d04d847b1370ad4df634aa6327c9ed
sealanguage <elaine.froehlich@gmail.com>
2020-07-17 20:13:01 -0400

    learning to print dictionaries using indentation formatting and not using indentation formatting

---
+++ print_dictionary.py
@@ -0,0 +1,4 @@
+population = {"india": 13670979441, "china": 1419518156, "US": 3288308848}
+
+for key in population:
+    print(key, population[key])
---
+++ print_dictionary_w_indentation.py
@@ -0,0 +1,4 @@
+population = {"india": 1367097341, "china": 1419518156, "US": 328830848}
+
+for key in population:
+    print("{:<10} - {:>10}".format(key, population[key]))
